d184799033629a37f45c6131525ca8e54e0c0067
jessicagallagher <jessicalyngallagher@me.com>
2021-05-11 20:09:28 -0400

    tailwind setup finished

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'green-default': '#1cd3a2',
+        'green-light': '#03ffc8',
+        'pink-default': '#f05c85',
+        'pink-light': '#ffb6ce',
+      },
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
