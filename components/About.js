be5c5dfddef4684300149e66e170705029d6106f
jessicagallagher <jessicalyngallagher@me.com>
2021-08-20 08:00:29 -0400

    to switch computers

--- components/About.js
+++ components/About.js
@@ -1,6 +1,19 @@
 export default function About() {
   return (
-    <div className='max-w-7xl px-4 mx-auto sm:px-6 lg:px-8 mt-14'>
+    <div className='max-w-7xl px-4 mx-auto sm:px-6 lg:px-8 mt-4'>
+      <div className='bg-pink-light overflow-hidden shadow sm:rounded-lg mt-28 z-20'>
+        <div className='px-4 py-5 sm:p-6 flex flex-row'>
+          <div className='align-middle'>
+              <h1 className='font-lato font-bold text-black text-6xl align-middle'>About Me</h1>
+            </div>
+            
+            <img className='h-40 w-40 rounded-sm xl:w-1/3 xl:h-1/3 align-middle' src='./images/jessica.jpg' alt='cartoon jessica' />
+        </div>
+          
+
+      </div>
+
+
       <img className='mx-auto h-40 w-40 rounded-sm xl:w-1/3 xl:h-1/3 border border-pink-default' src='./images/jessica.jpg' alt='cartoon jessica' />
       
       <p className='mx-auto mt-3 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl font-lato text-black'>I'm originally from a small town in Louisiana called Houma. It's way down at the bottom of the boot and around 45 minutes from New Orleans. I started coding as a hobby. My first language was Ruby, and I only learned it because it was the new cool language at the time. Over the course of 5 years, I taught myself how to code basic websites and applications by volunteering with several non-profits, and after 14 years of working in various management consulting roles, I decided to make a career change so that I could finally do something that I was really passionate and excited about. So, here I am!</p>
