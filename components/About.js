ab5a46a22f2086f8c4aec83cbcbe9c218933c33f
jessicagallagher <jessicalyngallagher@me.com>
2021-08-20 05:33:47 -0400

    stopping point

--- components/About.js
+++ components/About.js
@@ -1,20 +1,13 @@
 export default function About() {
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-14'>
-      <div className='px-4 py-5 sm:p-6'>
-        <img className='mx-auto h-40 w-40 rounded-sm xl:w-80 xl:h-80 border border-pink-default' src='./images/jessica.jpg' alt='cartoon jessica' />
-      </div>
+    <div className='max-w-7xl px-4 mx-auto sm:px-6 lg:px-8 mt-14'>
+      <img className='mx-auto h-40 w-40 rounded-sm xl:w-1/3 xl:h-1/3 border border-pink-default' src='./images/jessica.jpg' alt='cartoon jessica' />
+      
       <p className='mx-auto mt-3 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl font-lato text-black'>I'm originally from a small town in Louisiana called Houma. It's way down at the bottom of the boot and around 45 minutes from New Orleans. I started coding as a hobby. My first language was Ruby, and I only learned it because it was the new cool language at the time. Over the course of 5 years, I taught myself how to code basic websites and applications by volunteering with several non-profits, and after 14 years of working in various management consulting roles, I decided to make a career change so that I could finally do something that I was really passionate and excited about. So, here I am!</p>
-      <p className='mx-auto mt-3 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl font-lato text-black'></p>
+      <p className='mx-auto mt-3 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl font-lato text-black'>What drew me to engineering was the feeling of solving one giant problem (Did I mention that I'm a Virgo?). There's no right or wrong way, and you can take multiple paths to get there. Languages are always evolving and changing--so the learning never stops. While I call myself a full-stack engineer, I enjoy front-end the most. Don't get me wrong--routing, databases, and all of the behind-the-scenes goodness is super cool, but I like the instant gratification feeling of hitting save, refreshing my browser, and seeing what I've built. Unless it's an error message. I don't like that!</p>
       <p className='mx-auto mt-3 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl font-lato text-black'>When I'm not writing code for work, I'm usually writing code for fun! You can also find me exploring Carroll Gardens, knitting things that I never finish, running, and reading. I'm a Yankees fan, Saints fan, and bleed purple and gold for my LSU Tigers.</p>
       
 
     </div>
   )
-}
-
-{
-  /* <p className='mt-3 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-lato text-black'>I'm originally from a small town in Louisiana called Houma. It's way down at the bottom of the boot and around 45 minutes from New Orleans. I started coding as a hobby. My first language was Ruby, and I only learned it because it was the new cool language at the time. Over the course of 5 years, I taught myself how to code basic websites and applications by volunteering with several non-profits, and after 14 years of working in various management consulting roles, I decided to make a career change so that I could finally do something that I was really passionate and excited about. So, here I am!</p>
-  <p className='mt-3 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-lato text-black'>When I'm not writing code for work, I'm usually writing code for fun! You can also find me exploring Carroll Gardens, knitting things that I never finish, running, and reading. I'm a Yankees fan, Saints fan, and bleed purple and gold for my LSU Tigers.</p>
-  <p className = 'mt-3 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-lato text-black'>Oh, also I've been into some light home improvement...</p> */
 }
