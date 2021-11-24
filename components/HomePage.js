25cda817d3b244c0e3501840f2feac6050390a76
jessicagallagher <jessicalyngallagher@me.com>
2021-11-23 21:45:47 -0500

    fix gradient

--- components/HomePage.js
+++ components/HomePage.js
@@ -44,11 +44,13 @@ export default function HomePage() {
 						type="button"
 						className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 					>
-						<MailIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-						Get in touch!
+						<div className='-ml-1 mr-3 -mt-2.5 h-5 w-5 text-2xl'>
+							👋
+						</div>
+						Say hi!
 					</button>
 				</div>
-				
+
 			</div>
 		</div>
 	)
