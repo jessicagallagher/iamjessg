8f3953d3bec62359a8ac5952e80a10c1657599f1
jessicagallagher <jessicalyngallagher@me.com>
2021-12-14 11:24:23 -0500

    build

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 
 const dev = PHASE_DEVELOPMENT_SERVER;
+const prod = PHASE_PRODUCTION_BUILD;
 
 export default class MyDocument extends Document {
   render() {
