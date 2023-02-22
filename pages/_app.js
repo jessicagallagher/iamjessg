e90c2470493428c2cf37ccccae68afe7a1442193
jessicagallagher <jessicalyngallagher@me.com>
2023-02-22 11:06:55 -0500

    add gtm to test on prod

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-MSV5GHB' });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
