2850bcbfeb560a7f3b113c3041d166b6be452497
jessicagallagher <jessicalyngallagher@me.com>
2023-02-22 11:47:52 -0500

    remove gtm module and use vanilla

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,45 @@
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
+import * as gtag from '../utils/gtag'
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-MSV5GHB' });
-  }, []);
+  const router = useRouter();
 
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id=G-PW6JJFKKP6'
+      />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PW6JJFKKP6', {
+              page_path: window.location.pathname,
+            });
+            `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
