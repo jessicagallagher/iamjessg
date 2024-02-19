46b24eca39ebfbc8dd7fa6d8ec3b2a6d60fed0b8
jessicagallagher <jessicalyngallagher@me.com>
2024-02-19 11:25:39 -0500

    add instructions where needed and remove unused image

--- components/PortfolioJessicaGallagher.jsx
+++ components/PortfolioJessicaGallagher.jsx
@@ -1,6 +1,5 @@
 import img from '../public/images/github-logo.png';
 import webImg from '../public/images/website.png';
-import constructionImg from '../public/images/under-construction.png';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -12,21 +11,25 @@ export default function PortfolioJessicaGallagher() {
       techUsed: 'Next.js, Tailwind CSS, Calendly, SendGrid',
       projectUrl: 'https://techmeowt.com',
       projectGitHubUrl: 'https://github.com/jessicagallagher/iamjessg/tree/dev',
+      projectInstructions: '',
     },
     {
       projectName: 'Realtor.com Clone',
       imageUrl: '/images/portfolio-images/realtor.png',
-      techUsed: 'React, Tailwind CSS, Firebase, Leaflet, Toastify + Swiper, OAuth',
+      techUsed:
+        'React, Tailwind CSS, Firebase Firestore, Firebase Authentication, Leaflet, Toastify + Swiper, OAuth',
       projectUrl: 'https://realtor-clone-tan.vercel.app/',
       projectGitHubUrl: 'https://github.com/jessicagallagher/realtor-clone',
+      projectInstructions: 'This is a fully functioning web application that requires creating an account to post a real estate listing. The database will save your listings and your user account information, however, your user account information is private, encrypted, and will never be shared with any third parties.',
     },
     {
-      projectName: 'Database Management System *Demo Coming Soon!',
+      projectName: 'Database Management System',
       imageUrl: '/images/portfolio-images/database-management-system.gif',
       techUsed:
         'React, MongoDB, Express, Node, Prisma ORM, Nodemailer, JWT Authentication, Bootstrap, + Reactstrap',
       projectUrl: '#',
       projectGitHubUrl: 'https://github.com/Tech-Meowt/red2blue-v1',
+      projectInstructions: 'This project is only accessible by the client. A demo of this web application is coming soon.',
     },
     {
       projectName: 'Candy Crush Clone',
@@ -34,6 +37,7 @@ export default function PortfolioJessicaGallagher() {
       techUsed: 'React',
       projectUrl: 'https://jess-candy-crush.vercel.app/',
       projectGitHubUrl: 'https://github.com/jessicagallagher/react-candy-crush',
+      projectInstructions: 'This game is accessible on both web and mobile browsers. Use your mouse to move the candies if you\'re using the web version, and utilize the touch capabilities on your phone if using the mobile version.',
     },
     {
       projectName: 'Tetris',
@@ -41,6 +45,7 @@ export default function PortfolioJessicaGallagher() {
       techUsed: 'React',
       projectUrl: 'https://jess-tetris.vercel.app/',
       projectGitHubUrl: 'https://github.com/jessicagallagher/tetris',
+      projectInstructions: 'This game is accessible on web browsers only. Hold the down arrow key to quickly drop the tetromino, press the left and right arrow keys to position the teromino, and press the up arrow key to rotate the tetromino.',
     },
     {
       projectName: 'Where To?',
@@ -49,13 +54,15 @@ export default function PortfolioJessicaGallagher() {
         'Next.js, Tailwind CSS, Firebase Authentication, + Firebase Firestore',
       projectUrl: 'https://where-to.vercel.app/',
       projectGitHubUrl: 'https://github.com/jessicagallagher/where_to_v2',
+      projectInstructions: 'This is a fully functioning web application that requires users to create an account. Your account information is saved in a database and is private, encrypted, and will never be shared with any third parties.',
     },
     {
       projectName: 'Weather App',
       imageUrl: '/images/portfolio-images/weather-app.png',
-      techUsed: 'React, OpenWeather API',
+      techUsed: 'React + OpenWeather API',
       projectUrl: 'https://jess-react-weather-app.vercel.app/',
       projectGitHubUrl: 'https://github.com/jessicagallagher/react-weather-app',
+      projectInstructions: '',
     },
   ];
 
@@ -74,13 +81,13 @@ export default function PortfolioJessicaGallagher() {
           {projects.map((project) => (
             <li key={project.projectName}>
               <div className='space-y-4'>
-                    <div className='aspect-w-3 aspect-h-2'>
-                      <img
-                        className='rounded-lg object-scale-down shadow-lg border border-pinkDefault hover:shadow-2xl hover:border-greenDefault'
-                        src={project.imageUrl}
-                        alt={`Screenshot of project: ${project.projectName}`}
-                      />
-                    </div>
+                <div className='aspect-w-3 aspect-h-2'>
+                  <img
+                    className='rounded-lg object-scale-down shadow-lg border border-pinkDefault hover:shadow-2xl hover:border-greenDefault'
+                    src={project.imageUrl}
+                    alt={`Screenshot of project: ${project.projectName}`}
+                  />
+                </div>
                 <div className='space-y-2'>
                   <div className='space-y-1 font-medium leading-6'>
                     <Link legacyBehavior href={project.projectUrl}>
@@ -89,7 +96,9 @@ export default function PortfolioJessicaGallagher() {
                         target='_blank'
                         rel='noopener'
                       >
-                        <h3 className='text-lg md:text-xl'>{project.projectName}</h3>
+                        <h3 className='text-lg md:text-xl'>
+                          {project.projectName}
+                        </h3>
                       </a>
                     </Link>
                     <p>Built with {project.techUsed}</p>
@@ -130,12 +139,19 @@ export default function PortfolioJessicaGallagher() {
                           </a>
                         </Link>
                       ) : (
-                        <Image
-                          src={constructionImg}
-                          width={30}
-                          height={30}
-                          aria-hidden='true'
-                        />
+                        ''
+                      )}
+                    </li>
+                  </ul>
+                  <ul role='list'>
+                    <li>
+                      {project.projectInstructions !== '' ? (
+                        <p className='text-pinkDefault font-semibold'>
+                          <span className='text-3xl'>👀 </span>
+                          {project.projectInstructions}
+                        </p>
+                      ) : (
+                        ''
                       )}
                     </li>
                   </ul>
