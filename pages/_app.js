fd31be25cc5fd8adbdc76f3ce2057f806294d9b6
jessicagallagher <jessicalyngallagher@me.com>
2024-10-01 15:09:42 -0400

    fixed env

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import '../styles/globals.css';
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <GoogleTagManager gtmId={process.env.GTM_ID} />
-      <GoogleAnalytics gaId={process.env.GA_ID} />
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </>
   );
 }
