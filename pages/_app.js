d2ee44cd01fe62f8383fc0fdd6d4bb54555d3f46
jessicagallagher <jessicalyngallagher@me.com>
2024-10-01 13:04:37 -0400

    jesus take the wheel

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css';
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <GoogleTagManager gtmId={process.env.GTM_ID} />
+      <GoogleAnalytics gaId={process.env.GA_ID} />
     </>
   );
 }
