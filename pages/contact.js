e03dc16473cd44ca36a43c232e6dc054b4054ed2
jessicagallagher <jessicalyngallagher@me.com>
2025-09-22 16:03:14 -0400

    add all keywords

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,18 @@
 import Head from 'next/head'
-import { ContactUs, NavBar, HeadTag, Footer } from '../components'
+import { ContactUs, NavBar, Footer } from '../components'
 
 export default function Contact() {
   return (
     <div>
       <Head>
+        <meta name='description' content="Tech Meowt's website" />
+        <meta
+          name='keywords'
+          content='HTML, CSS, JavaScript, React, Next, Web Developer, Software Engineer, Software Developer, Tailwind, Brooklyn, Non Profit, Tech Meowt, Agency, Branding, CRM, Blog, Database'
+        />
+        <meta name='author' content='Jessica Gallagher' />
         <title>Contact Us</title>
         <link rel='icon' href='/favicon.ico' />
-        <HeadTag />
       </Head>
       <main>
         <NavBar />
