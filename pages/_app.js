8e8d91f54bc951e28bbd8c5b90c516b272c1dafa
jessicagallagher <jessicalyngallagher@me.com>
2025-10-07 13:04:04 -0400

    add vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </>
