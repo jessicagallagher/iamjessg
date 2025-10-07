76c7ef2c57af696fd91d8fd0b9bd6fe58cbc18c1
jessicagallagher <jessicalyngallagher@me.com>
2025-10-07 13:10:06 -0400

    try to fix build error

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <>
       <Component {...pageProps} />
-      <SpeedInsights />
+      <SpeedInsights route={router.pathname}/>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </>
