5e287e571d2653229259c1e5f77293407e926406
jessicagallagher <jessicalyngallagher@me.com>
2025-11-18 13:01:46 -0500

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/next'
 import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
       <SpeedInsights route={router.pathname}/>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
